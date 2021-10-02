c7b159912f4e4255594b4db81b9732e26014ac30
eir <esteban@eir.local>
2021-10-02 11:03:22 -0500

    Class 3.1 - Digital Inputs and Outputs

---
+++ Class3-1_DigitalIO/src/main.cpp
@@ -0,0 +1,28 @@
+/*
+
+LAS TERMINALES 34 A 39 NO TIENEN RESISTENCIA PULL UP O DOWN Y SOLO PUEDEN SER USADAS COMO ENTRADAS
+
+*/
+
+#include <Arduino.h>
+
+#define pinIn 18
+#define pinOut 2
+
+void setup() {
+  /*  
+
+      Utiliza la resistencia pull down integrada dentro del módulo esp32,
+      en caso de usar una resistencia externa, colocar una en  el circuito
+      y reemplazar INPUT_PULLDOWN por INPUT.
+
+      Existe la definición INPUT_PULLUP y INPUT_PULLDOWN
+  
+  */
+  pinMode(pinIn, INPUT_PULLDOWN);
+  pinMode(pinOut, OUTPUT);
+}
+
+void loop() {
+  digitalRead(pinIn) ? digitalWrite(pinOut, HIGH) : digitalWrite(pinOut, LOW);
+}
