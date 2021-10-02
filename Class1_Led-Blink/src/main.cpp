18a0173e5dab6f6f7087b1b1e8fba4442aca5bd0
Esteban Ibarra <sectorjeat@gmail.com>
2021-10-02 00:18:45 -0500

    esp32 udemy course class 3 project 1

---
+++ Class1_Led-Blink/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define pinLed 2
+
+void setup() {
+  pinMode(pinLed, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(pinLed, HIGH);
+  delay(1000);
+  digitalWrite(pinLed, LOW);
+  delay(1000);
+}
