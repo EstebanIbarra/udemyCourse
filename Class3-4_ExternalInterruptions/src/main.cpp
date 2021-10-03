d1974d03d44afc3ac3bfbadec17c70c6e0dab70a
eir <esteban@eir.local>
2021-10-03 16:17:46 -0500

    Class 3.4 - External Interruptions

---
+++ Class3-4_ExternalInterruptions/src/main.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+
+/*
+
+  => Las interrupciones externas son eventos que disparan la ejecución 
+    de alguna sección de código dependiendo de algún cambio en alguna terminal
+  
+  => Existen 3 tipos de  interrupciones:
+    -> Rising   Cuando una terminal está en estado lógico 0 y pasa a 1
+    -> Falling  Cuando una terminal está en estado lógico 1 y pasa a 0
+    -> Change   Cuando una terminal cambia de estado lógico sin importar el estado previo
+
+*/
+
+#define pinINTERRUPTION 23
+#define pinOUT 2
+#define rebounceTime 200
+
+/*
+
+  El calificador volatile le indica al compilador que el valor de esta variable puede cambiar en cualquier momento.
+  TODAS LAS VARIABLES A UTILIZAR EN LAS INTERRUPCIONES DEBEN TENER EL CALIFICADOR VOLATILE
+  https://www.tutorialspoint.com/what-is-volatile-keyword-in-cplusplus
+
+*/
+
+volatile bool toggle = true;
+volatile unsigned long previousInterruptionTime  = 0;
+
+/*  ==> FUNCTION DECLARATION <==  */
+void IRAM_ATTR interruption();    // La palabra rerrseervada IRAM_ATTR se refiere a que la función se almacene en la memoria RAM en lugar de la flash, para acelerar la ejecución del progrrama
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinOUT, OUTPUT);
+  pinMode(pinINTERRUPTION, INPUT_PULLUP);
+  attachInterrupt(                            // Función necesaria para configurar una interrupción
+    digitalPinToInterrupt(pinINTERRUPTION),   // Función que declara que la terminal designada en el argumento va a ser utilizada como interrupción
+    interruption,                             // Función a ejecutar en caso de una interrupción
+    FALLING                                   // Tipo de evento de interrupción
+  );
+}
+
+void loop() {
+  Serial.println("Message");
+  delay(500);
+}
+
+/*  ==> FUNCTION DEFINITION <== */
+void IRAM_ATTR interruption() {
+  if (millis() - previousInterruptionTime > rebounceTime){    // millis() cuenta el tiempo desde que el ESP32 encendió
+    digitalWrite(pinOUT, toggle);
+    Serial.println("Interruption");
+    toggle ^= true;     // ^ es un operador XOR binario: copia el bit de información si está presente en un operando pero no en ambos. https://www.tutorialspoint.com/cplusplus/cpp_operators.htm
+    previousInterruptionTime = millis();
+  }
+}
