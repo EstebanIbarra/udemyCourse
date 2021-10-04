d843be35a2f502d48ba90a001fb8184255c3a30a
eir <esteban@eir.local>
2021-10-04 00:03:50 -0500

    Class 4.1 - UART

---
+++ Class4-1_UART/README.md
@@ -0,0 +1,11 @@
+            ===>    UART Y COMUNICACIÓN SERIAL    <===
+
+La comunicación serial (UART) es un protocolo bastante común utilizado en la comunicación entre dispositivos.
+
+La conexión serial entre dispositivos se logra siguiendo la siguiente conexión:
+
+    * Dispositivo 1 - Tx    ->  Dispositivo 2 - Rx
+    * Dispositivo 1 - Rx    ->  Dispositivo 2 - Tx
+    * Dispositivo 1 - GND   ->  Dispositivo 2 - GND
+
+Estas conexiones entre el ESP32 y la computadora ya están integradas, por lo que no es necesario hacerlas
---
+++ Class4-1_UART/src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+int value;
+
+void setup() {
+  Serial.begin(115200);
+
+  /* Serial.print */
+  Serial.print("--- Serial.print ---");
+  Serial.print("--- Serial.print2 ---");
+
+  /* Serial.println */
+  Serial.println("\n");
+  Serial.println("--- Serial.println ---\n");
+  Serial.println(117);
+  Serial.println(123.123);    // Por defecto trunca el número de decimales a 2
+  Serial.println('E');
+  Serial.println("String");
+
+  /* Serial.println con segundo argumento */
+  Serial.println("\n\n--- Serial.println con segundo argumento ---\n");
+  Serial.println("Las siguientes líneas imprimen el número 117 en distintas bases:\nDEC, HEX, BIN y OCT");
+  Serial.println(117, DEC);
+  Serial.println(117, HEX);
+  Serial.println(117, BIN);
+  Serial.println(117, OCT);
+  Serial.println("\nLas siguientes líneas imprimen el número 123.123 con distinto truncado de decimales");
+  Serial.println(123.123, 0);
+  Serial.println(123.123, 3);
+}
+
+void loop() {
+  while (Serial.available()) {    // Serial.available retorna un bool si existen valores en el buffer
+    /**
+     * Comment/Uncomment the next code block to test Serial.readString
+     **/
+  /*
+    value = Serial.read();        // Serial.read lee un byte del buffer y retorna su valor entero
+    Serial.println(value);
+    Serial.write(value);
+    Serial.println();
+  */
+    Serial.println(Serial.readString());
+  }
+  delay(100);
+}
