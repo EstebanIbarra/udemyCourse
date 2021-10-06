dcd5aeff69ca0fc9f2f2aa35da1e780878ff5c6e
eir <esteban@eir.local>
2021-10-06 00:01:16 -0500

    Class 4.4 - Json Serialization

---
+++ Class4-4_JsonSerialization/README.md
@@ -0,0 +1,5 @@
+            ===>    SERIALIZACIÓN CON JSON    <===
+
+Al proceso de recopilar varias variables o datos dentro de un solo mensaje para su envío/recepción se le conoce como serialización. En este caso utilizaremos el formato JSON para serializar información diversa dentro de un solo mensaje.
+
+JSON significa JavaScript Object Notation (Notación de Objetos de JavaScript), y es uno de los formatos de serialización más ampliamente difundidos y utilizados junto con XML, sin embargo, a diferencia del formato XML, este formato es más ligero y sencillo puesto que no depende del demarcado de etiquetas para su funcionamiento
---
+++ Class4-4_JsonSerialization/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+StaticJsonDocument<192> jsonMessage;    // Variable de tipo StaticJsonDocument con un tamaño de 192 bytes
+String jsonMessageString;
+/* https://arduinojson.org/v6/assistant   ->  Calculadora en línea para el tamaño ideal para los objettos de tipo StaticJsondocument */
+
+int var1 = 123;
+float var2 = 123.123;
+bool var3 = true;
+String var4 = "Hello there";
+
+void setup() {
+  Serial.begin(115200);
+
+  /* Almacenamos las variables dentro del Json */
+  jsonMessage["var1"].set(var1);    // .set() e = son equivalentes
+  jsonMessage["var2"].set(var2);
+  jsonMessage["var3"] = var3;
+  jsonMessage["var4"] = var4;
+
+  JsonObject nestedJsonMessage = jsonMessage.createNestedObject("Nested");  // Genera un objeto anidado dentro de nuestro json original
+
+  nestedJsonMessage["nestedVar1"] = 123.123;
+  nestedJsonMessage["nestedVar2"] = "Some string";
+  nestedJsonMessage["nestedVar3"] = false;
+
+  //serializeJson(jsonMessage, jsonMessageString);          // Método que serializa nuestro objeto json en una cadena de texto y lo asigna a la variable jsonMessageString
+  serializeJsonPretty(jsonMessage, jsonMessageString);    // La diferencia entre serializeJson y serializeJsonPretty solo es el formato del mensaje, uno cuenta con saltos de línea e indentaciones mientras el otro no
+
+}
+
+void loop() {
+  Serial.println(jsonMessageString);
+  delay(15000);
+}
