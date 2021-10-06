359008f2e90f8311d556ab86836c2b8fd50c9a17
eir <esteban@eir.local>
2021-10-05 23:21:47 -0500

    Class 4.3 - WiFi (STA)

---
+++ Class4-3_WiFiSTA/README.md
@@ -0,0 +1,9 @@
+            ===>    WIFI COMO ESTACIÓN    <===
+
+Existen tres tipos de configuraciones para el ESP32:
+
+    -> Estación (STA)           El dispositivo se conecta a una red WiFi disponible
+    -> Punto de Acceso (AP)     El dispositivo genera su propia red WiFi a la que se pueden conectar otros dispositivos
+    -> Ambos
+
+/** TODO: Serial.print dentro de void setup no funciona */
---
+++ Class4-3_WiFiSTA/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+#define SSID "insert your SSID"
+#define PWD "insert your password"
+
+void initWiFi();
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+}
+
+void loop() {}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);            // WiFi en modo Estación
+  //WiFi.mode(WIFI_AP);           // WiFi en modo Punto de Acceso
+  //WiFi.mode(WIFI_MODE_APSTA);   // WiFi en ambos modos
+
+  WiFi.begin(SSID, PWD);          // Inicia la conexión WiFi con las credenciales mostradas previamente
+  Serial.print("Connecting to: ");
+  Serial.print(SSID);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(50);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println('\n');
+    Serial.println("Succesful connection");
+  }
+
+  Serial.print("\nYour IP Address is: ");
+  Serial.println(WiFi.localIP());
+}
